181ceed508b0f97f1438638ce37d4bf8826a333e
Daniel Geisler <daniellgeisler@gmail.com>
2024-05-29 06:11:09 -0700

    Comparison between 2020 and 2024

--- nlab_conll.py
+++ nlab_conll.py
@@ -4,7 +4,7 @@ from tqdm import tqdm
 import spacy
 from spacy_conll import init_parser
 
-os.chdir('/NetMath/nlab/data')
+os.chdir('/NetMath/nLab/data')
 if os.path.exists('nlab_new.json'):
     os.remove('nlab_new.json')
 
@@ -14,40 +14,55 @@ conll_parser = init_parser(
     "en_core_web_sm", 
     "spacy", 
     #include_headers=True,
-    ext_names={"conll_pd": "pandas"},
+    #ext_names={"conll_pd": "pandas"},
 )
-#nlp.add_pipe("conll_formatter", last=True)
+
 nlp.max_length = 15000000
 
 page = ""
-with open('nlab_clean.json', 'r', encoding="utf-8") as input, open('nlab_new.json', 'w', encoding="utf-8") as output:
+
+# New output portion to generate CoNLL-U-like format
+def get_conllu_format(doc):
+    clines = []
+    for sent_id, sent in enumerate(doc.sents, start=1):
+        clines.append(f"# sent_id = {sent_id}")
+        clines.append(f"# text = {sent.text}")
+        for token_id, token in enumerate(sent, start=1):
+            line = [
+                token_id,  # ID
+                token.text,  # FORM
+                token.lemma_,  # LEMMA
+                token.pos_,  # UPOS
+                token.tag_,  # XPOS
+                "_",  # FEATS (can be replaced with actual features)
+                token.head.i - token.sent[0].i + 1 if token.head != token else 0,  # HEAD
+                token.dep_,  # DEPREL
+                "_",  # DEPS
+                "_" if token.whitespace_ else "SpaceAfter=No"  # MISC
+            ]
+            clines.append("\t".join(map(str, line)))
+        clines.append("")
+    return "\n".join(clines)
+
+with open('nlab_clean.json', 'r', encoding="utf-8") as input, open('nlab2024.conll', 'w', encoding="utf-8") as output:
     lines = []   
     items = list(range(1800000))
     cnt = 0
     for item in tqdm(items):
         rec = input.readline()
-        if "PAGINATE" in rec:
-            data = ''.join(lines)    
+        if "{" in rec:
+            data = ''.join(lines)
             data = re.sub(' - ', ' ', data)
+            data = re.sub('\n', ' ', data)            
             data = re.sub(r'\s+', ' ', data)
-            data = re.sub('PAGINATE', ' ', data)            
+            data = re.sub(r'{', '\n', data)            
             doc = conll_parser(data)
-            conll = doc._.conll_str
-            #conll = doc._.pandas
-            if cnt > 0:
-                output.write("PAGINATE\n")
-            article = "# page = " + page + "\n"
-            output.write(article)
-            output.write(conll)
+            conllu_output = get_conllu_format(doc)
+            output.write(conllu_output)
             lines = []
             cnt += 1
-        elif "page = " in rec:
-            match = re.search("page = (.*)", rec)
-            if match:
-                page = match.group(1)
-            rec = re.sub(r'page = .*', '', rec)
+        elif "}" in rec:
+            rec = ""
         elif rec == "":
             break           
-        lines.append(rec) 
-
-
+        lines.append(rec)
